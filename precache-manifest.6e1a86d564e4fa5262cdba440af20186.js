71264e87f0e920c1f4ba1c38cd0520c5de862821
unknown <you@example.com>
2019-09-16 12:10:10 +0200

    Updates

---
+++ precache-manifest.6e1a86d564e4fa5262cdba440af20186.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f3da843785a21842007e50125b9946a",
+    "url": "/Movie-app-/index.html"
+  },
+  {
+    "revision": "cc537dfba3b415731ead",
+    "url": "/Movie-app-/static/css/main.560c6e50.chunk.css"
+  },
+  {
+    "revision": "bc34289a7e99d97d2147",
+    "url": "/Movie-app-/static/js/2.585d8b2b.chunk.js"
+  },
+  {
+    "revision": "cc537dfba3b415731ead",
+    "url": "/Movie-app-/static/js/main.bc599638.chunk.js"
+  },
+  {
+    "revision": "ab79251d02c323e09358",
+    "url": "/Movie-app-/static/js/runtime~main.f401f371.js"
+  },
+  {
+    "revision": "8e06191e709008df05691f79a4a4eb4e",
+    "url": "/Movie-app-/static/media/AQuitPlace.8e06191e.png"
+  },
+  {
+    "revision": "f7a9cddb5351921a9b4924cd46cbab49",
+    "url": "/Movie-app-/static/media/BridBox.f7a9cddb.png"
+  },
+  {
+    "revision": "8a4bec410e9e9e738e6b8fe2f6a9fdc8",
+    "url": "/Movie-app-/static/media/EscapeRom.8a4bec41.jpg"
+  },
+  {
+    "revision": "aee17703f3634e088fb2f24344a09cf1",
+    "url": "/Movie-app-/static/media/GameOfThrone.aee17703.png"
+  },
+  {
+    "revision": "1475a81fb686c826dda69fac0c756475",
+    "url": "/Movie-app-/static/media/Plus.1475a81f.png"
+  },
+  {
+    "revision": "db9833bb8f5f5b938857829908934094",
+    "url": "/Movie-app-/static/media/ToyStory.db9833bb.jpg"
+  }
+]);
